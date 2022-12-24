f915be6be2a63c2b6172c424e56ef8acf077095c
Dauleshwar Kumar <dauleshwar.kumar@freecharge.com>
2022-12-24 18:22:55 +0530

    added redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import { Counter } from './Counter';
 
 function App() {
   return (
     <div className="App">
       <h1>Working on aws</h1>
+      <Counter/>
     </div>
   );
 }
